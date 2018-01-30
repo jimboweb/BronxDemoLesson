b1b3cbaa7ec0198eb0a39dc788f01530f1d494d9
jimboweb <jimboweb@gmail.com>
2018-01-29 21:48:33 -0500

    mean median mode program

---
+++ meanmedianmode/MeanMedianMode.py
@@ -0,0 +1,46 @@
+import math
+
+siblingData = [2, 0, 3, 0, 1, 4, 3, 2, 1, 1, 2, 1, 3, 2, 1, 1, 2, 5, 2, 3, 4, 2, 4]
+
+total = 0
+for number in siblingData:
+    total = total + number
+
+print("our total number of siblings are " + str(total))
+
+numberCount = len(siblingData)
+
+average = total/numberCount
+average = round(average,2)
+
+print("on average we have " + str(average) + " siblings")
+
+sortedNumbers = sorted(siblingData)
+
+minSiblings = str(sortedNumbers[0])
+print("the smallest number of siblings is " + minSiblings)
+maxSiblings = sortedNumbers[numberCount-1]
+print("the largest number of siblings is " + str(maxSiblings))
+
+siblingCount = []
+
+for i in range(0,maxSiblings+1):
+    siblingCount.append(0)
+
+for number in siblingData:
+    siblingCount[number]+=1
+
+siblingMode = siblingCount.index(max(siblingCount))
+
+print("the mode of our number of siblings is " + str(siblingMode))
+
+median = 0
+if numberCount%2==0:
+    firstMiddle = int(numberCount/2)
+    secondMiddle = firstMiddle+1
+    median = int((sortedNumbers[firstMiddle] + sortedNumbers[secondMiddle])/2)
+else:
+    middleNumber = math.ceil(numberCount/2) + 1
+    median = sortedNumbers[middleNumber]
+
+print("our median number of siblings is " + str(median))
